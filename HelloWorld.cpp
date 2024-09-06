1e288e80dd85a514fad49c5831a4ffc97b40b394
A1bic <leokov3009@gmail.com>
2024-09-06 11:10:33 +0300

    Helloworld

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,5 +4,8 @@ int main()
 {
     std::cout << "Hello world!" << std::endl;
     std::cout << "Привет, мир!" << std::endl;
+    std::cout << "Привет, мир!" << std::endl;
+    std::cout << "Привет, мир!" << std::endl;
+    std::cout << "Привет, мир!" << std::endl;
     return 0;
 }
