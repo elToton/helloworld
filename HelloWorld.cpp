a4f25175de22063757422cb2d9607a49461c69be
ARTSPRN <artyom-suprun-yndx@yandex.ru>
2024-09-06 11:08:51 +0300

    BubleSort

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
-int main()
-{
-    //Hello 
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+int main(){
+    int arr[] = {5,2,7,28,72,0,-1};
+    int n = sizeof(arr) / sizeof(*arr);
+    for(int a = 0; a < n-1;a++)
+        for(int b = 0; b < n - a - 1; b++)
+            if (arr[b + 1] > arr[b])
+                std::swap(arr[b+1],arr[b]);
+    for(int c = 0; c < n; c++)
+        std::cout << arr[c] <<  " ";    
 }
