c2c18c27a55b419a66e5386a9746fe72f8a8c077
karim <nursafin_karim@mail.ru>
2024-09-07 09:52:52 +0300

    loop i = 0 to 100

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
+    int n = 100;
+    vector<int> arr(n);
 
-    for (int i = 0; i < 100; ++i) {
-        std::cout << i * i << "\n";
+    for (int i = 0; i < n; ++i) {
+        arr[i] = i * i;
     }
 
+    for (int i = 0; i < arr.size(); ++i) {
+        cout << arr[i] << "\n";
+    }
 
     return 0;
 }
